4073d182b6ff9ff4eeea6a260cf1e523597499fb
Axxu <134137941+AxxuDEV@users.noreply.github.com>
2023-06-21 18:59:07 +0300

    refactor

--- esx_aj_banking/sv_banking.lua
+++ esx_aj_banking/sv_banking.lua
@@ -1,16 +1,16 @@
 RegisterNetEvent('esx_aj_banking:sv:nosto', function(amount)
     local xPlayer = ESX.GetPlayerFromId(source)
     local balance = xPlayer.getAccount('bank').money
-    if xPlayer then 
-        if balance > amount then 
-            xPlayer.removeAccountMoney('bank', amount)
-            xPlayer.addAccountMoney('money', amount)
-        else
-            lib.notify(source, {
-                description = 'Sinulla ei ole tarpeeksi pankissa rahaa',
-                type = 'error',
-            })
-        end
+    if not xPlayer then return end
+        
+    if balance > amount then 
+        xPlayer.removeAccountMoney('bank', amount)
+        xPlayer.addAccountMoney('money', amount)
+    else
+        lib.notify(source, {
+            description = 'You dont have enough money...',
+            type = 'error',
+        })
     end
 end)
 
@@ -18,7 +18,7 @@ RegisterNetEvent('esx_aj_banking:sv:saldo', function()
     local xPlayer = ESX.GetPlayerFromId(source)
     local balance = xPlayer.getAccount('bank').money
     lib.notify(source, {
-        description = 'Sinulla on '..balance..'$ pankissa',
+        description = 'You Have '..balance..'$ in the bank',
         type = 'inform',
     })
 end)
@@ -44,38 +44,46 @@ RegisterNetEvent('esx_aj_banking:sv:siirto', function(id, amount)
     local player = ESX.GetPlayerFromId(source)
     local receiver = ESX.GetPlayerFromId(id)
     local balance = player.getAccount('bank').money
-    if player then
-        if receiver ~= nil then 
-            if tonumber(source) ~= tonumber(id) then
-                if balance ~= nil then 
-                    player.removeAccountMoney('bank', amount)
-                    receiver.addAccountMoney('bank', amount)
-                    TriggerClientEvent('ox_lib:notify', receiver, {
-                        description = 'Sait rahaa henkilöltä ' ..player.getName()..' summan : '..amount.. '$',
-                        type = 'success',
-                    })
-                    lib.notify(source, {
-                        description = 'Siirsit henkilölle ' ..receiver.getName()..' summan : '..amount.. '$',
-                        type = 'success',
-                    })
-               else 
-                    lib.notify(source, {
-                        description = 'Sinulla ei ole tarpeeksi rahaa',
-                        type = 'error',
-                   }) 
-                end
-            else
-                lib.notify(source, {
-                    description = 'Et voi siirtää itsellesi rahaa',
-                    type = 'error',
-                })
-            end
-        else
-            lib.notify(source, {
-                description = 'Ei henkilöä tuolla id:llä',
-                type = 'error',
-            })
-        end
+    if not player then 
+        return 
     end
+    if not receiver then
+        lib.notify(source, {
+            description = 'Ei henkilöä tuolla id:llä',
+            type = 'error',
+        })
+        return    
+    end
+
+    if tonumber(source) == tonumber(id) then
+        lib.notify(source, {
+            description = 'Et voi siirtää itsellesi rahaa',
+            type = 'error',
+        })
+        return
+    end
+
+    if balance == nil then 
+        return
+    end
+
+    if balance < amount then 
+        lib.notify(source, {
+            description = 'Sinulla ei ole tarpeeksi rahaa',
+            type = 'error',
+        })     
+        return
+    end
+        
+    player.removeAccountMoney('bank', amount)
+    receiver.addAccountMoney('bank', amount)
+    TriggerClientEvent('ox_lib:notify', receiver, {
+        description = 'You received money from: ' ..player.getName()..' in the amount of : '..amount.. '$',
+        type = 'success',
+    })
+    lib.notify(source, {
+        description = 'You transferred money to ' ..receiver.getName()..' in the amount of : '..amount.. '$',
+        type = 'success',
+    })
 end)
 
